ddba167a90b61cd10f96525fc7e0149779446f57
Liam Thursfield <liam3006x@gmail.com>
2024-05-22 00:18:50 +0100

    Update web content resource to map json fields for easier use

--- app/Http/Resources/Web/CMS/ContentResource.php
+++ app/Http/Resources/Web/CMS/ContentResource.php
@@ -13,13 +13,20 @@ class ContentResource extends BaseResource
     {
         $data = $this->data;
         if (in_array($this->template_field_type, TemplateFieldInterface::JSON_TYPES)) {
-            $data = json_decode($data, true);
+            $data = collect(json_decode($data, true));
+
+            if ($this->template_field_type === TemplateFieldInterface::TYPE_COMPONENT) {
+                $data = $data->keyBy('template_field_slug');
+            } else if ($this->template_field_type === TemplateFieldInterface::TYPE_REPEATER) {
+                $data = $data->map(function($item) {
+                    return collect($item)->keyBy('template_field_slug');
+                });
+            }
         }
 
         return [
             'data'              => $data,
             'template_field'    => TemplateFieldResource::make($this->whenLoaded('templateField')),
-
         ];
     }
 }
